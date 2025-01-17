3a865efd53993da37e6e5dd3f083080a651c6067
JoabeChaves <chavesjoabe1@gmail.com>
2025-01-17 19:42:05 -0300

    feat: remove unused files and update security configs

--- src/main/java/com/treasury/treasury/security/SecurityConfig.java
+++ src/main/java/com/treasury/treasury/security/SecurityConfig.java
@@ -20,8 +20,8 @@ public class SecurityConfig {
         .csrf((csrf) -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("api/user/login").permitAll()
-            .requestMatchers("api/balance/health").permitAll()
+            .requestMatchers("/api/user/login").permitAll()
+            .requestMatchers("/api/balance/health").permitAll()
             .anyRequest().authenticated()
             ).cors();
 
